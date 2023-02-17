d56fe2f0249393958dc6440467c99e64c7bf720d
0fly98 <lundujosh97@gmail.com>
2023-02-16 17:36:48 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - printing the string with printful function
+ * Return: program return 0 (Success)
  */
 int main(void)
 {
