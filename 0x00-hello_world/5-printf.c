c2e24c97d0c25806a20be6edfc6f56a26cc06312
0fly98 <lundujosh97@gmail.com>
2023-02-16 18:04:35 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - printing the string with printful function
- * Return: program return 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
