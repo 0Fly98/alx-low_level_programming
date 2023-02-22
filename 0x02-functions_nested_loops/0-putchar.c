6dc5fddfa4a7783732982dc532b8c7e7fe2f2dd7
0fly98 <lundujosh97@gmail.com>
2023-02-22 15:34:20 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _Putchar and a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_Putchar('u');
+	_Putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_Putchar('a');
+	_Putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
