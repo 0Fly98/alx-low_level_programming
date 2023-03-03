517e19b45cffd93e00344c6cb60eaeff2dfe046d
0fly98 <lundujosh97@gmail.com>
2023-03-03 02:12:38 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,29 +3,30 @@
 #include <unistd.h>
 
 /**
- * _strcat - Concatenates two strings.
- * @dest: The destination string.
- * @src: The source string.
+ * _strcat - concatenates two strings
+ * @dest: destination string to append to
+ * @src: source string to append from
  *
- * Return: A pointer to the resulting string dest.
+ * Return: pointer to resulting string dest
  */
 char *_strcat(char *dest, char *src)
 {
-    int dest_len = 0;
-    int i;
+	char *ptr = dest;
 
-    /* Find the length of the destination string */
-    while (dest[dest_len] != '\0')
-        dest_len++;
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
 
-    /* Concatenate the source string to the destination string */
-    for (i = 0; src[i] != '\0'; i++)
-        dest[dest_len + i] = src[i];
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
 
-    /* Add the terminating null byte */
-    dest[dest_len + i] = '\0';
+	*ptr = '\0';
 
-    /* Return the resulting string */
-    return dest;
+	return (dest);
 }
 
--- 0x06-pointers_arrays_strings/main.c
+++
@@ -1,9 +0,0 @@
-// main.c
-
-int _strcmp(char *s1, char *s2);
-
-int main(void)
-{
-    // ...
-}
-
