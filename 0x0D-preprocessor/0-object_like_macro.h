46052672b0b1ec070162e2b488109d4c7002bfb4
0fly98 <lundujosh97@gmail.com>
2023-03-20 16:43:31 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,11 +1,6 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
-/**
- * Macro: SIZE
- *----------------------------
- *An abbreviation token of 1024.
- */
 #define SIZE 1024
 
-#endif /*OBJECT_LIKE_MACRO_H*/
+#endif
