1c43a54e4289351b7b9531ed7954e5cec7b58f45
0fly98 <lundujosh97@gmail.com>
2023-03-21 12:11:34 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 #include "dog.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    struct dog my_dog;
-
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	struct dog my_dog;
+	init_dog(&my_dog, "poppy", 3.5, "Bob");
+	printf("My dog's name is %s, age is %f, and owner is %s\n", my_dog.name, my_dog.age, my_dog.owner);
+	return 0;
 }
