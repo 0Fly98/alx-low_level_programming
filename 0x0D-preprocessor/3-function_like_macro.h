8112b621507fe0bb383c6cfd0462d01ea1cdf948
0fly98 <lundujosh97@gmail.com>
2023-03-21 05:57:28 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
-#ifndef MY_ABS_MACRO_H
-#define MY_ABS_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-/*This macro returns the absolute value of x*/
-#define ABS(x) ((x < 0) ? (-x) : (x))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
-#endif /*MY_ABS_MACRO_H*/
+#endif
