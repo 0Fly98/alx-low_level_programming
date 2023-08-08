cb808a9088c7f069c606b6bc95212fdeb394d92e
josh <lundujosh97@gmail.com>
2023-08-08 09:31:51 -0700

    libmy.a

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,17 +1,9 @@
 #include "main.h"
-#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-/**
- * _strcmp - Compares two strings.
- *
- * @S1: The first string to be compared.
- * @s2: The second string to be compared.
- *
- * Return: A negative value if @s1 is less than @s2,
- *	a positive value if @s1 is greater than @s2.
- */
-char *_memset(char *s1, char *s2)
+char *_strcmp(char *s1, char*s2)
 {
-	return memset(s1, s2);
+	return strcmp(s1, s2);
 }
