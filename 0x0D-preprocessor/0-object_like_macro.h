3faf888f8b62edc5417297fc2a969e2adae7832f
0Fly98 <lundujosh97@gmail.com>
2023-09-11 06:13:37 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,8 +1,7 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
-/* Define SIZE as an enum */
-enum
-{ SIZE = 1024 };
+/* Macro definition */
+#define SIZE 1024
 
 #endif /* OBJECT_LIKE_MACRO_H */
