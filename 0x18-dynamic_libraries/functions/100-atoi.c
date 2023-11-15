daf3e3e1769fae376bfe94a43dec45d2adb407f4
0Fly98 <lundujosh97@gmail.com>
2023-11-15 09:09:16 -0800

    A library is not a luxury

---
+++ 0x18-dynamic_libraries/functions/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to the convert
+ *
+ * Return: the converted integer value
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
